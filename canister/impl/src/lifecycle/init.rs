629f86ba36f8b92415f8f52fa8b7809a99363657
Hamish Peebles <hamishpeebles@gmail.com>
2024-04-29 14:10:36 +0100

    Allow specifying the RNG salt for tests

--- canister/impl/src/lifecycle/init.rs
+++ canister/impl/src/lifecycle/init.rs
@@ -8,28 +8,34 @@ use std::time::Duration;
 #[init]
 fn init(args: InitOrUpgradeArgs) {
     let init_args = args.to_init_args();
+    let test_mode = init_args.salt.is_some();
 
-    state::init(State::new(init_args.test_mode));
+    state::init(State::new(test_mode));
 
-    if init_args.test_mode {
+    if let Some(salt) = init_args.salt {
         email_sender::init(NullEmailSender::default());
+        set_salt(salt, 0)
+    } else {
+        ic_cdk_timers::set_timer(Duration::ZERO, || {
+            ic_cdk::spawn(async {
+                let salt: [u8; 32] = ic_cdk::api::management_canister::main::raw_rand()
+                    .await
+                    .unwrap()
+                    .0
+                    .try_into()
+                    .unwrap();
+
+                set_salt(salt, env::now());
+            })
+        });
     }
+}
 
-    ic_cdk_timers::set_timer(Duration::ZERO, || {
-        ic_cdk::spawn(async {
-            let salt: [u8; 32] = ic_cdk::api::management_canister::main::raw_rand()
-                .await
-                .unwrap()
-                .0
-                .try_into()
-                .unwrap();
+fn set_salt(salt: [u8; 32], entropy: u64) {
+    rng::set_seed(salt, entropy);
 
-            rng::set_seed(salt, env::now());
-
-            state::mutate(|s| {
-                s.set_rsa_private_key(rng::generate_rsa_private_key());
-                s.set_salt(salt);
-            });
-        })
+    state::mutate(|s| {
+        s.set_rsa_private_key(rng::generate_rsa_private_key());
+        s.set_salt(salt);
     });
 }
